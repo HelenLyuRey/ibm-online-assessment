3458a7a4f197e675f59fa586b9fff2181e3c7583
HelenLyuRey <helenlyurey888@qq.com>
2022-11-11 23:09:08 +0800

    Correct Q2

--- Q2.py
+++ Q2.py
@@ -1,13 +1,33 @@
-import pandas as pd
+# Q2 DEBUG0001. Dataset Validity
 
-df = pd.read_csv("data.txt", sep=" ",skiprows=1,header=None)
-df2 = df.set_axis(['index','status', 'err_msg'], axis=1, inplace=False)
+with open('data.txt') as topo_file:
+    allValid = True
+    errorCodes = []
+    next(topo_file)
+    for line in topo_file:
+        new_line = line.rstrip('\n')
+        line_list = new_line.split(' ')
+        if (line_list[1] == 'false'):
+            allValid = False
+            errorCodes.append(line_list[2])
 
-status = all(df2['status']==True)
-
-if (status == True):
+if (allValid == True):
     print("Yes")
 else:
     print("No")
-    msg_list = [x for x in list(df2['err_msg']) if str(x) != 'nan']
-    print(' '.join(msg_list))
+    print(' '.join(errorCodes))
+
+# Second way, different from pseudo-code
+# import pandas as pd
+
+# df = pd.read_csv("data.txt", sep=" ",skiprows=1,header=None)
+# df2 = df.set_axis(['index','status', 'err_msg'], axis=1, inplace=False)
+
+# status = all(df2['status']==True)
+
+# if (status == True):
+#     print("Yes")
+# else:
+#     print("No")
+#     msg_list = [x for x in list(df2['err_msg']) if str(x) != 'nan']
+#     print(' '.join(msg_list))
